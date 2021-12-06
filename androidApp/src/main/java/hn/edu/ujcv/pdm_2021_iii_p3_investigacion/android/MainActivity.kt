bc074e3684e0dcb4706299b3e5fc0c5e91254c9f
andrea-munoz-alcantara <andrea.munoz@ujcv.edu.hn>
2021-12-06 16:36:51 -0600

    Initial commit

---
+++ androidApp/src/main/java/hn/edu/ujcv/pdm_2021_iii_p3_investigacion/android/MainActivity.kt
@@ -0,0 +1,19 @@
+package hn.edu.ujcv.pdm_2021_iii_p3_investigacion.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hn.edu.ujcv.pdm_2021_iii_p3_investigacion.Greeting
+import android.widget.TextView
+
+fun greet(): String {
+    return Greeting().greeting()
+}
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
